914d0644bc500d7a8deaec68e74785cc26c7d6b1
Armel Sahamene <saharmel100@gmail.com>
2017-04-12 15:45:23 -0400

    fixed rotating cards

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -17,54 +17,179 @@
     <AppCarousel />
     <div class="barre" />
 
-    <!--Rotating card-->
-    <div class="card-wrapper">
-      <div id="card-1" class="card-rotating effect__click">
+    <!--Component title-->
+    <h5 class="text-center mt-3 mb-2">Rotating cards</h5>
 
-        <!--Front Side-->
-        <div class="face front">
+    <!--First row-->
+    <div class="row">
 
-          <!-- Image-->
-          <div class="card-up">
-            <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%2859%29.jpg" class="img-fluid">
-          </div>
-          <!--Avatar-->
-          <div class="avatar"><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%289%29.jpg" class="rounded-circle img-responsive">
-          </div>
-          <!--Content-->
-          <div class="card-block">
-            <h4>Jonathan Doe</h4>
-            <p>Web developer</p>
-            <!--Triggering button-->
-            <a class="rotate-btn" data-card="card-1"><i class="fa fa-repeat"></i> Click here to rotate</a>
+      <!--First column-->
+      <div class="col-lg-4 col-md-12">
+
+        <!--Rotating card-->
+        <div class="card-wrapper">
+          <div id="card-1" class="card-rotating effect__click">
+
+            <!--Front Side-->
+            <div class="face front">
+
+              <!-- Image-->
+              <div class="card-up">
+                <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%2859%29.jpg" class="img-fluid">
+              </div>
+              <!--Avatar-->
+              <div class="avatar"><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%289%29.jpg" class="rounded-circle img-responsive">
+              </div>
+              <!--Content-->
+              <div class="card-block">
+                <h4>Jonathan Doe</h4>
+                <p>Web developer</p>
+                <!--Triggering button-->
+                <a class="rotate-btn" data-card="card-1"><i class="fa fa-repeat test-icon"></i> Click here to rotate</a>
+              </div>
+            </div>
+            <!--/.Front Side-->
+
+            <!--Back Side-->
+            <div class="face back">
+
+              <!--Content-->
+              <h4>About me</h4>
+              <hr>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime quae, dolores dicta. Blanditiis rem amet repellat, dolores nihil quae in mollitia asperiores ut rerum repellendus, voluptatum eum, officia laudantium quaerat?</p>
+              <hr>
+              <!--Social Icons-->
+              <ul class="inline-ul">
+                <li><a class="icons-sm fb-ic"><i class="fa fa-facebook"></i></a></li>
+                <li><a class="icons-sm git-ic"><i class="fa fa-github"></i></a></li>
+                <li><a class="icons-sm gplus-ic"><i class="fa fa-google-plus"></i></a></li>
+                <li><a class="icons-sm li-ic"><i class="fa fa-linkedin"></i></a></li>
+              </ul>
+              <!--Triggering button-->
+              <a class="rotate-btn" data-card="card-1"><i class="fa fa-undo"></i> Click here to rotate back</a>
+
+            </div>
+            <!--/.Back Side-->
+
           </div>
         </div>
-        <!--/.Front Side-->
-
-        <!--Back Side-->
-        <div class="face back">
-
-          <!--Content-->
-          <h4>About me</h4>
-          <hr>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime quae, dolores dicta. Blanditiis rem amet repellat, dolores nihil quae in mollitia asperiores ut rerum repellendus, voluptatum eum, officia laudantium quaerat?</p>
-          <hr>
-          <!--Social Icons-->
-          <ul class="inline-ul">
-            <li><a class="icons-sm fb-ic"><i class="fa fa-facebook"></i></a></li>
-            <li><a class="icons-sm tw-ic"><i class="fa fa-twitter"></i></a></li>
-            <li><a class="icons-sm gplus-ic"><i class="fa fa-google-plus"></i></a></li>
-            <li><a class="icons-sm li-ic"><i class="fa fa-linkedin"></i></a></li>
-          </ul>
-          <!--Triggering button-->
-          <a class="rotate-btn" data-card="card-1"><i class="fa fa-undo"></i> Click here to rotate back</a>
-
-        </div>
-        <!--/.Back Side-->
+        <!--/.Rotating card-->
 
       </div>
+      <!--/First column-->
+
+      <!--Second column-->
+      <div class="col-lg-4 col-md-6">
+
+        <!--Rotating card-->
+        <div class="card-wrapper">
+          <div id="card-2" class="card-rotating effect__click">
+
+            <!--Front Side-->
+            <div class="face front">
+
+              <!-- Image-->
+              <div class="card-up">
+                <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(7).jpg" class="img-fluid">
+              </div>
+              <!--Avatar-->
+              <div class="avatar"><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg" class="rounded-circle img-responsive">
+              </div>
+              <!--Content-->
+              <div class="card-block">
+                <h4>Marie Clark</h4>
+                <p>Web designer</p>
+                <!--Triggering button-->
+                <a class="rotate-btn" data-card="card-2"><i class="fa fa-repeat"></i> Click here to rotate</a>
+              </div>
+            </div>
+            <!--/.Front Side-->
+
+            <!--Back Side-->
+            <div class="face back">
+
+              <!--Content-->
+              <h4>About me</h4>
+              <hr>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime quae, dolores dicta. Blanditiis rem amet repellat, dolores nihil quae in mollitia asperiores ut rerum repellendus, voluptatum eum, officia laudantium quaerat?</p>
+              <hr>
+              <!--Social Icons-->
+              <ul class="inline-ul">
+                <li><a class="icons-sm fb-ic"><i class="fa fa-facebook"></i></a></li>
+                <li><a class="icons-sm tw-ic"><i class="fa fa-twitter"></i></a></li>
+                <li><a class="icons-sm gplus-ic"><i class="fa fa-google-plus"></i></a></li>
+                <li><a class="icons-sm dribbble-ic"><i class="fa fa-dribbble"></i></a></li>
+              </ul>
+              <!--Triggering button-->
+              <a class="rotate-btn" data-card="card-2"><i class="fa fa-undo"></i> Click here to rotate back</a>
+
+            </div>
+            <!--/.Back Side-->
+
+          </div>
+        </div>
+        <!--/.Rotating card-->
+
+      </div>
+      <!--/Second column-->
+
+      <!--Third column-->
+      <div class="col-lg-4 col-md-6">
+
+        <!--Rotating card-->
+        <div class="card-wrapper">
+          <div id="card-3" class="card-rotating effect__click">
+
+            <!--Front Side-->
+            <div class="face front">
+
+              <!-- Image-->
+              <div class="card-up">
+                <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(2).jpg" class="img-fluid">
+              </div>
+              <!--Avatar-->
+              <div class="avatar"><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(17).jpg" class="rounded-circle img-responsive">
+              </div>
+              <!--Content-->
+              <div class="card-block">
+                <h4>Kate Smith</h4>
+                <p>Photographer</p>
+                <!--Triggering button-->
+                <a class="rotate-btn" data-card="card-3"><i class="fa fa-repeat"></i> Click here to rotate</a>
+              </div>
+            </div>
+            <!--/.Front Side-->
+
+            <!--Back Side-->
+            <div class="face back">
+
+              <!--Content-->
+              <h4>About me</h4>
+              <hr>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime quae, dolores dicta. Blanditiis rem amet repellat, dolores nihil quae in mollitia asperiores ut rerum repellendus, voluptatum eum, officia laudantium quaerat?</p>
+              <hr>
+              <!--Social Icons-->
+              <ul class="inline-ul">
+                <li><a class="icons-sm fb-ic"><i class="fa fa-facebook"></i></a></li>
+                <li><a class="icons-sm pin-ic"><i class="fa fa-pinterest"></i></a></li>
+                <li><a class="icons-sm ins-ic"><i class="fa fa-instagram"></i></a></li>
+                <li><a class="icons-sm tw-ic"><i class="fa fa-twitter"></i></a></li>
+              </ul>
+              <!--Triggering button-->
+              <a class="rotate-btn" data-card="card-3"><i class="fa fa-undo"></i> Click here to rotate back</a>
+
+            </div>
+            <!--/.Back Side-->
+
+          </div>
+        </div>
+        <!--/.Rotating card-->
+
+      </div>
+      <!--/Third column-->
+
     </div>
-    <!--/.Rotating card-->
+    <!--/First row-->
 
   </div>
 </template>
@@ -75,9 +200,16 @@
 
 export default {
   name: 'HomePage',
+  mounted() {
+    $(".rotate-btn").on("click", function () {
+      var t = $(this).attr("data-card");
+      $("#" + t).toggleClass("flipped")
+    })
+  },
   components: {
       AppCarousel
   }
+
 }
 </script>
 
@@ -131,20 +263,6 @@ export default {
     text-decoration: none;
   }
 
-  .people {
-    display: inline-block;
-    margin-bottom: 20px;
-    max-width: 20%;
-    margin-right: 1%;
-    margin-left: 1%;
-    padding-bottom: 10px;
-  }
-
-  .people-image {
-    border-radius: 30%;
-    margin-bottom: 20px;
-  }
-
   p {
     color: black;
   }
@@ -155,9 +273,6 @@ export default {
     margin-bottom: 30px;
     background-color: black;
   }
-  .container{
-    margin-left: 10%;
-    margin-right: 10%;
-    text-align: center;
-  }
+
+
 </style>
