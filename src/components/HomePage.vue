aacfd31969b6438991134ad40a7f681c1ee9850f
Armel Sahamene <saharmel100@gmail.com>
2017-04-12 15:17:37 -0400

    added carousel

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,52 +1,72 @@
 <template>
-  <!--Rotating card-->
-  <div class="card-wrapper">
-    <div id="card-1" class="card-rotating effect__click">
-
-      <!--Front Side-->
-      <div class="face front">
-
-        <!-- Image-->
-        <div class="card-up">
-          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%2859%29.jpg" class="img-fluid">
-        </div>
-        <!--Avatar-->
-        <div class="avatar"><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%289%29.jpg" class="rounded-circle img-responsive">
-        </div>
-        <!--Content-->
-        <div class="card-block">
-          <h4>Jonathan Doe</h4>
-          <p>Web developer</p>
-          <!--Triggering button-->
-          <a class="rotate-btn" data-card="card-1"><i class="fa fa-repeat"></i> Click here to rotate</a>
-        </div>
-      </div>
-      <!--/.Front Side-->
-
-      <!--Back Side-->
-      <div class="face back">
-
-        <!--Content-->
-        <h4>About me</h4>
-        <hr>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime quae, dolores dicta. Blanditiis rem amet repellat, dolores nihil quae in mollitia asperiores ut rerum repellendus, voluptatum eum, officia laudantium quaerat?</p>
-        <hr>
-        <!--Social Icons-->
-        <ul class="inline-ul">
-          <li><a class="icons-sm fb-ic"><i class="fa fa-facebook"></i></a></li>
-          <li><a class="icons-sm tw-ic"><i class="fa fa-twitter"></i></a></li>
-          <li><a class="icons-sm gplus-ic"><i class="fa fa-google-plus"></i></a></li>
-          <li><a class="icons-sm li-ic"><i class="fa fa-linkedin"></i></a></li>
-        </ul>
-        <!--Triggering button-->
-        <a class="rotate-btn" data-card="card-1"><i class="fa fa-undo"></i> Click here to rotate back</a>
-
-      </div>
-      <!--/.Back Side-->
-
+  <div>
+    <div class="logoB">
+      <img src="../assets/logo2.png" alt="notreBoite" class="logo">
     </div>
+    <div id="bar" class="clearfix">
+      <div id="menu">
+        <ul class="clearfix">
+          <li id="current"><a href="index.html">Home</a></li>
+          <li ><a href="index.html">Archives</a></li>
+          <li><a href="index.html">Profile</a></li>
+          <li><a href="index.html">Followers</a></li>
+          <li><a href="index.html">Support</a></li>
+        </ul>
+      </div>
+    </div>
+    <AppCarousel />
+    <div class="barre" />
+
+    <!--Rotating card-->
+    <div class="card-wrapper">
+      <div id="card-1" class="card-rotating effect__click">
+
+        <!--Front Side-->
+        <div class="face front">
+
+          <!-- Image-->
+          <div class="card-up">
+            <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%2859%29.jpg" class="img-fluid">
+          </div>
+          <!--Avatar-->
+          <div class="avatar"><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%289%29.jpg" class="rounded-circle img-responsive">
+          </div>
+          <!--Content-->
+          <div class="card-block">
+            <h4>Jonathan Doe</h4>
+            <p>Web developer</p>
+            <!--Triggering button-->
+            <a class="rotate-btn" data-card="card-1"><i class="fa fa-repeat"></i> Click here to rotate</a>
+          </div>
+        </div>
+        <!--/.Front Side-->
+
+        <!--Back Side-->
+        <div class="face back">
+
+          <!--Content-->
+          <h4>About me</h4>
+          <hr>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime quae, dolores dicta. Blanditiis rem amet repellat, dolores nihil quae in mollitia asperiores ut rerum repellendus, voluptatum eum, officia laudantium quaerat?</p>
+          <hr>
+          <!--Social Icons-->
+          <ul class="inline-ul">
+            <li><a class="icons-sm fb-ic"><i class="fa fa-facebook"></i></a></li>
+            <li><a class="icons-sm tw-ic"><i class="fa fa-twitter"></i></a></li>
+            <li><a class="icons-sm gplus-ic"><i class="fa fa-google-plus"></i></a></li>
+            <li><a class="icons-sm li-ic"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+          <!--Triggering button-->
+          <a class="rotate-btn" data-card="card-1"><i class="fa fa-undo"></i> Click here to rotate back</a>
+
+        </div>
+        <!--/.Back Side-->
+
+      </div>
+    </div>
+    <!--/.Rotating card-->
+
   </div>
-  <!--/.Rotating card-->
 </template>
 
 <script>
